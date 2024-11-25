93445e9c5346ed79e5744c5ec195290dbe08616a
Michael Rogov <mihaillogin5@gmail.com>
2024-11-25 23:51:55 +0300

    добавление метода create_buttons

--- calc.py
+++ calc.py
@@ -12,3 +12,42 @@ class ScientificCalculator:
         self.input_field.grid(row=0, column=0, columnspan=5, sticky="we")
         # Кнопки
         self.create_buttons()
+
+   def create_buttons(self):
+        buttons = [
+            ("7", 1, 0),
+            ("8", 1, 1),
+            ("9", 1, 2),
+            ("/", 1, 3),
+            ("C", 1, 4),
+            ("4", 2, 0),
+            ("5", 2, 1),
+            ("6", 2, 2),
+            ("*", 2, 3),
+            ("sqrt", 2, 4),
+            ("1", 3, 0),
+            ("2", 3, 1),
+            ("3", 3, 2),
+            ("-", 3, 3),
+            ("log", 3, 4),
+            ("0", 4, 0),
+            (".", 4, 1),
+            ("^", 4, 2),
+            ("+", 4, 3),
+            ("=", 4, 4),
+            ("sin", 5, 0),
+            ("cos", 5, 1),
+            ("tan", 5, 2),
+            ("pi", 5, 3),
+            ("e", 5, 4),
+        ]
+
+        for text, row, col in buttons:
+            tk.Button(
+                self.root,
+                text=text,
+                font=("Arial", 18),
+                bd=5,
+                command=lambda t=text: self.on_button_click(t),
+            ).grid(row=row, column=col, sticky="we", padx=5, pady=5)
+     
